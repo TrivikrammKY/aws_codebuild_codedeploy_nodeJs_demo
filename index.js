98c1359300a678d478320610f50ba404cb125c67
leenatejababu <76763210+leenatejababu@users.noreply.github.com>
2022-11-12 02:05:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V2!')
+  res.send('Welcome to Valuemomentum : V1')
 })
 
 app.get('/status', (req, res) => {
